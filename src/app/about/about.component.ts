f9ecc8ae90569a7b90172234e54a7fe14c1aca13
Matthew Bishop <matthbish@gmail.com>
2024-09-18 10:49:14 -0400

    Update skills

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,19 +12,19 @@ export class AboutComponent implements OnInit {
   skills: Skill[] = [
     {
       label: 'Languages',
-      arr: ['Java', 'Kotlin', 'TypeScript', 'JavaScript', 'HTML', 'CSS/SCSS', 'Python', 'SQL', 'R', 'C#', 'Assembly', 'C', 'F#', 'MATLAB']
+      arr: ['Java', 'JavaFX', 'Kotlin', 'TypeScript', 'JavaScript', 'HTML', 'CSS/SCSS', 'Python', 'SQL', 'Pascal', 'Powershell', 'Bash', 'R', 'C#', 'Assembly', 'C', 'F#', 'MATLAB']
     },
     {
       label: 'Frameworks',
-      arr: ['Spring Boot', 'Axon', 'Angular', 'Jest', 'REST', 'MongoDB', 'RabbitMQ', 'React', 'Aeon', 'CICD', 'CQRS']
+      arr: ['Spring Boot', 'Axon', 'Angular', 'Jest', 'REST', 'MongoDB', 'MariaDB', 'RabbitMQ', 'Node.js', 'React', 'Aeon', 'CICD', 'CQRS', 'Swagger']
     },
     {
       label: 'Software',
-      arr: ['IntelliJ', 'VS Code', 'Docker', 'Postman', 'Robo3T/Studio3T', 'Lens', 'Android Studio', 'Eclipse', 'NetBeans', 'Wireshark', 'Ubuntu', 'Unity']
+      arr: ['IntelliJ', 'VS Code', 'Docker', 'Postman', 'Robo3T/Studio3T', 'Lens', 'Android Studio', 'Inno Setup', 'DBeaver', 'GitHub', 'GitLab', 'JIRA', 'Fortify', 'Eclipse', 'NetBeans', 'Wireshark', 'Ubuntu', 'Unity']
     },
     {
       label: 'Operating Systems',
-      arr: ['MacOS', 'Windows', 'Android', 'Linux', ]
+      arr: ['MacOS', 'Windows', 'Android', 'Linux', 'RHEL' ]
     }
   ];
 
